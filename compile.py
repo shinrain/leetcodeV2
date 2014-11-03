29755a4e9e1b2590ba66a51d02cf132232e9b06d
personal <shinrainxie@gmail.com>
2014-11-02 21:10:00 -0800

    LAZY: some more interesting things

--- compile.py
+++ compile.py
@@ -7,14 +7,15 @@ if __name__ == "__main__":
 
 	filename = open("oj","w");
 	sl_path = os.getcwd()+"/code/";
-
+	count = 1;
 	delimite = "\n========================================\n"
 	for file in os.listdir(sl_path):
 		if file.endswith(".git"):
 			continue
 		file = sl_path+file
 		filename.write(delimite)
-		filename.write(file+delimite+"\n"+"\n");
+		filename.write("("+str(count)+"). "+file+delimite+"\n"+"\n");
 		filename.write(open(file,"r").read())
 		filename.write("\n\n")
+		count = count+1
 	filename.close()
